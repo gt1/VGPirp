98da2aad921deadc17eadc04dcb3d70656b1a283
tischler <tischler@mpi-cbg.de>
2019-02-21 10:14:58 +0100

    add header production in samToPBR

---
+++ VGP_BRPHeader.hpp
@@ -0,0 +1,120 @@
+/**
+    Copyright (C) 2009-2019 German Tischler
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+**/
+#if ! defined(BRPHEADER_HPP)
+#define BRPHEADER_HPP
+
+#include <map>
+#include "VGP_FileType.hpp"
+#include "VGP_Provenance.hpp"
+#include "VGP_BRPReaderException.hpp"
+#include "VGP_MaxNumberPrint.hpp"
+
+struct BRPHeader
+{
+	FileType FT;
+	std::vector<ProvenanceStep> VP;
+	std::map<char, std::map<char,vgp_number_type > > headerMap;
+
+	BRPHeader() {}
+	BRPHeader(LineBuffer & LB)
+	{
+		FT = FileType(LB);
+		// P = Provenance(LB);
+		readHeaderLines(LB);
+	}
+
+	void readHeaderLines(LineBuffer & LB)
+	{
+		char const * la = NULL;
+		char const * le = NULL;
+
+		while ( LB.getline(&la,&le) )
+		{
+			char const * linestart = la;
+
+			if ( la != le && !isalpha(*la) )
+			{
+				char const category = *(la++);
+
+				if ( la == le || *la != ' ' )
+				{
+					throw BRPReaderException(std::string("BRPReader::readHeaderLines: no space after plus in ") + std::string(linestart,le));
+				}
+				la++;
+
+				if ( category == '!' )
+				{
+					ProvenanceStep PS(la,le);
+					VP.push_back(PS);
+				}
+				else
+				{
+					if ( la == le )
+					{
+						throw BRPReaderException(std::string("BRPReader::readHeaderLines: no line type in ") + std::string(linestart,le));
+					}
+
+					char const linetype = *la++;
+
+					if ( la == le || *la != ' ' )
+					{
+						throw BRPReaderException(std::string("BRPReader::readHeaderLines: no space after line type type in ") + std::string(linestart,le));
+					}
+
+					++la;
+
+					vgp_number_type const n = BaseOp::getNumber(la,le);
+
+					headerMap[category][linetype] = n;
+				}
+			}
+			else
+			{
+				LB.putback(la,le);
+				break;
+			}
+		}
+	}
+
+	std::ostream & printHeader(std::ostream & out) const
+	{
+		out << FT;
+
+		for (
+			std::map<char, std::map<char,vgp_number_type > >::const_iterator it = headerMap.begin();
+			it != headerMap.end();
+			++it )
+		{
+			char const category = it->first;
+
+			std::map<char,vgp_number_type > const & M = it->second;
+
+			for ( std::map<char,vgp_number_type >::const_iterator sit = M.begin(); sit != M.end(); ++sit )
+			{
+				out << category << ' ' << sit->first << ' ' << MaxNumberPrint(sit->second) << '\n';
+			}
+		}
+
+		for ( uint64_t i = 0; i < VP.size(); ++i )
+			out << '!' << ' ' << VP[i] << "\n";
+
+		return out;
+	}
+};
+
+std::ostream & operator<<(std::ostream & out, BRPHeader const & H);
+#endif
--- VGP_samToPBR.cpp
+++ VGP_samToPBR.cpp
@@ -15,56 +15,144 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 **/
 #include "VGP_SAMReader.hpp"
+#include "VGP_BRPHeader.hpp"
+#include "VGP_GroupLineTools.hpp"
+#include "VGP_HeaderTools.hpp"
+#include <fstream>
 
-int main()
+int main(int argc, char * argv[])
 {
 	try
 	{
+		if ( argc < 2 )
+		{
+			std::cerr << "usage: " << argv[0] << " <out.brp> < in.sam\n";
+			return EXIT_FAILURE;
+		}
+
+		BRPHeader header;
+
+		header.FT.filetype = "brp";
+		header.FT.fileversion = 1;
+		header.FT.filesubversion = 0;
+
+		std::ostringstream comostr;
+		for ( uint64_t i = 0; i < argc; ++i )
+		{
+			if ( i )
+				comostr << ' ';
+			comostr << HeaderTools::quote(argv[i]);
+		}
+		std::string const com = comostr.str();
+
+		header.VP.push_back(
+			ProvenanceStep(
+				argv[0],
+				"0.0",
+				com,
+				"now"
+			)
+		);
+
+		header.headerMap['#']['L'] = 0;
+		header.headerMap['#']['S'] = 0;
+		header.headerMap['#']['A'] = 0;
+		header.headerMap['#']['N'] = 0;
+		header.headerMap['@']['S'] = 0;
+		header.headerMap['@']['A'] = 0;
+		header.headerMap['+']['S'] = 0;
+		header.headerMap['+']['A'] = 0;
+		header.headerMap['#']['!'] = 1;
+
+		#if 0
+		header.headerMap['#']['P'] = 0;
+		header.headerMap['#']['S'] = 0;
+		header.headerMap['@']['S'] = 0;
+		header.headerMap['+']['S'] = 0;
+		#endif
+
+		std::fstream out(argv[1], std::ios::in | std::ios::out | std::ios::trunc);
+
+		out << header;
+
 		SAMReader SR(std::cin);
+
+		vgp_number_type numL = 0;
+		vgp_number_type numS = 0;
+		vgp_number_type numA = 0;
+		vgp_number_type numN = 0;
+		vgp_number_type maxS = 0;
+		vgp_number_type maxA = 0;
+		vgp_number_type sumS = 0;
+		vgp_number_type sumA = 0;
 
 		PBRRecord * R = NULL;
 		while ( (R=SR.readData()) )
 		{
-			std::cout
+			out
 				<< 'L'
 				<< ' ' << R->well
 				<< ' ' << R->firstpulse
 				<< ' ' << R->lastpulse
 				<< ' ' << R->score
 				<< '\n';
-			std::cout << 'S'
+			numL += 1;
+			out << 'S'
 				<< ' ' << R->sequence_o
 				<< ' '
 				;
-			std::cout.write(
+			out.write(
 				R->sequence,
 				R->sequence_o
 			);
-			std::cout.put('\n');
+			out.put('\n');
+			numS += 1;
+			maxS = std::max(maxS,static_cast<vgp_number_type>(R->sequence_o));
+			sumS += R->sequence_o;
 
 			if ( R->N_o )
 			{
-				std::cout << 'N' << ' ';
-				std::cout.write(
+				out << 'N' << ' ';
+				out.write(
 					R->N,
 					R->N_o
 				);
 
-				std::cout.put('\n');
+				out.put('\n');
+
+				numN += 1;
 			}
 
 			if ( R->PW )
 			{
-				std::cout << 'A' << ' ' << R->PW_o << ' ';
-				std::cout.write(
+				out << 'A' << ' ' << R->PW_o << ' ';
+				out.write(
 					R->PW,
 					R->PW_o
 				);
-				std::cout << '\n';
+				out << '\n';
+
+				numA += 1;
+				sumA += R->PW_o;
+				maxA = std::max(maxA,static_cast<vgp_number_type>(R->PW_o));
 			}
 
 			// qs,qr,rq
 		}
+
+		header.headerMap['#']['L'] = numL;
+		header.headerMap['#']['S'] = numS;
+		header.headerMap['#']['N'] = numA;
+		header.headerMap['#']['A'] = numN;
+		header.headerMap['@']['S'] = maxS;
+		header.headerMap['@']['A'] = maxA;
+		header.headerMap['+']['S'] = sumS;
+		header.headerMap['+']['A'] = sumA;
+
+		out.seekp(0);
+		out << header;
+
+		out.flush();
 	}
 	catch(std::exception const & ex)
 	{
